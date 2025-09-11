d8d96d7f6f8977b65dfe8fe161982722df64bddb
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2025-09-11 17:29:38 -0300

    bugfix: ajustamento problema com as duplas

--- lua/josean/plugins/autopairs.lua
+++ lua/josean/plugins/autopairs.lua
@@ -1,31 +1,44 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local autopairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
 
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
-      },
-    })
+        -- Configuração base
+        autopairs.setup({
+            check_ts = true, -- usa Treesitter para contexto
+            ts_config = {
+                lua = { "string" }, -- não adicionar pares dentro de strings em Lua
+                javascript = { "template_string" },
+                typescript = { "template_string" },
+                javascriptreact = { "template_string" },
+                typescriptreact = { "template_string" },
+            },
+            fast_wrap = {}, -- ativa fast wrap (Ctrl-e por padrão)
+        })
 
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        -- Regras customizadas para evitar problemas com aspas em teclados ABNT2
+        autopairs.add_rules({
+            Rule('"', '"')
+                :with_pair(function(opts)
+                    -- só duplica aspas se o próximo caractere não for uma letra/número/aspas
+                    return opts.prev_char:match("[%w%s]") == nil
+                end),
 
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
+            Rule("'", "'")
+                :with_pair(function(opts)
+                    -- evita conflito de dead key em layouts ABNT2
+                    return opts.prev_char:match("[%w%s]") == nil
+                end),
+        })
 
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+        -- Integração com nvim-cmp
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
 }
-
